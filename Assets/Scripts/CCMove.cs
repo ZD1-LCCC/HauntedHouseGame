4b56a7bf4f20ef0c806f2023cfe9a2732e183a43
Zander DeLong <zdelong1@mymail.lccc.edu>
2024-10-22 19:32:15 -0400

    Player Movement
    Adds in a player object, movement with wasd, camera rotation, and simple geometry to move around in.

---
+++ Assets/Scripts/CCMove.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CCMove : MonoBehaviour
+{
+    private CharacterController _cc;   
+
+    public GameObject camera;
+    public float speed = 10.0f;
+    float gravity = -1f;
+    //float jumpForce = 500.0f;
+    public float sensTurn = 5.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _cc = GetComponent<CharacterController>();
+        camera = GameObject.Find("Main Camera");
+        camera.transform.position = gameObject.transform.position + new Vector3(0, 0.75f, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //turn when mouse left and right
+        transform.Rotate(0, Input.GetAxis("Mouse X")*sensTurn, 0);
+
+        float deltaX = Input.GetAxis("Horizontal")*speed;
+        float deltaZ = Input.GetAxis("Vertical")*speed;
+
+        Vector3 myDirection = new Vector3(deltaX, gravity, deltaZ);
+
+        //rotates the camera vertically with mouse
+        camera.transform.Rotate(Input.GetAxis("Mouse Y")*sensTurn*-1, 0, 0);
+        
+        //if statement for clamping vertical rotation? not working currently 
+        /*
+        if (camera.transform.rotation.x >= -90) {
+            camera.transform.rotation.x = -90;
+        }
+        else if (camera.transform.rotation.x >= 180) {
+            camera.transform.rotation.x = -90;
+        }
+
+        if (Input.GetButtonDown("Jump")) 
+        {
+            myDirection.y += jumpForce;
+        }   
+        */
+
+        //asjust for framerate
+        myDirection = myDirection * Time.deltaTime;
+
+        //limit max speed
+        myDirection = Vector3.ClampMagnitude(myDirection,speed);
+
+        //control direction to match game speed instead of object orientation
+        Vector3 myPlayerDirection = transform.TransformDirection(myDirection);
+
+        //actually moving the player, everything before is the logic for where to move
+        _cc.Move(myPlayerDirection);
+
+    }
+}
