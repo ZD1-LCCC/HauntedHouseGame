5520eaf14c84cb49364101fc7aeea6aca0a5519c
Zander DeLong <zdelong1@mymail.lccc.edu>
2024-10-31 10:11:02 -0400

    FIXED ROOM TEXT

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,23 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    public string[,] roomInfo = {
-        {"Room#1 Text#1", "Room#1 Text#2"},
-        {"Room#2 Text#1", "Room#2 Text#2"},
-        {"Room#3 Text#1", "Room#3 Text#2"},
-        {"Room#4 Text#1", "Room#4 Text#2"},
-        {"Room#5 Text#1", "Room#5 Text#2"},
-        {"Room#6 Text#1", "Room#6 Text#2"}
-    };
-
-
+    
     public static GameManager instance;
 
+    //public bool[,] objectStatus = {0};
+
+    public int[] numberEnter = {0,0,0,0,0,0};
+
+    public string[,] roomInfo = {
+        {"So this is where he disappeared to? Doesn’t look too haunted.","The trees are so dense, it’s like you are walled in.","Not yet"},
+        {"Is that a wall made of ghosts?","Seems like the only room I can access is the kitchen.","Not yet"},
+        {"An entire walk-in freezer, they must’ve been rich.","Starting to get hungry","Not yet"},
+        {"Anything good to read in here?","I'm surprised that the fireplace is working.","Not yet"},
+        {"What was that? Is someone down here?","I hope that banging sound wasn’t coming from down here.","Not yet"},
+        {"A graveyard next to a house? That is not a good sign.","The silence is deafening.","Not yet"}
+    };
+    
     void Awake() {
-        if (instance == null){
+        //to make one instance of the object and make it consistant between scenes
+        if (instance == null) {
+            
             instance = this;
         }
         else if (instance != this) {
@@ -25,6 +33,5 @@ public class GameManager : MonoBehaviour
         }
         DontDestroyOnLoad(gameObject);
     }
-
 
 }
--- Assets/Scripts/RoomController.cs
+++ Assets/Scripts/RoomController.cs
@@ -8,17 +8,23 @@ using TMPro;
 
 public class RoomController : MonoBehaviour
 {
+    private GameManager gameManager;
+
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
         //load text method for accesability?
         LoadText();
-
+        gameManager = GameManager.instance;
     }
+
+
 
     public void LoadText() {
         //to get scene name and other info?
         string sceneName = SceneManager.GetActiveScene().name;
+
+        int textNumb = 0;
 
         string sceneNumber = sceneName.Substring(sceneName.Length-1);
         Debug.Log("Room number is " + sceneNumber);
@@ -26,14 +32,29 @@ public class RoomController : MonoBehaviour
         int sceneNumb;
         sceneNumb = int.Parse(sceneNumber);
 
+        //to determine what level of text to show
+        if (GameManager.instance.numberEnter[sceneNumb-1] == 0) {
+            textNumb = 0;
+        }
+        else if (GameManager.instance.numberEnter[sceneNumb-1] > 0) {
+            textNumb = 1;
+        }
+        else {
+            textNumb = 1;
+        }
+        //increment the number of times a room was entered
+        ++GameManager.instance.numberEnter[sceneNumb-1];
+        
         //figure out the text
-        string myText = "";
+        string myText = GameManager.instance.roomInfo[sceneNumb-1,textNumb];
+        
 
         //actually changes the text
-        //GameObject.Find("RoomText").GetComponent<TextMeshProUGUI>().text = myText;
+        GameObject.Find("RoomText").GetComponent<TextMeshProUGUI>().text = myText;
 
         //looks into game manager to find the room info array and output the string for the text
-        GameObject.Find("RoomText").GetComponent<TextMeshProUGUI>().text = GameManager.instance.roomInfo[sceneNumb-1,0];
+        //GameObject.Find("RoomText").GetComponent<TextMeshProUGUI>().text = GameManager.instance.roomInfo[sceneNumb-1,0];
+
         
     }
 }
